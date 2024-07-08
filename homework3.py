dc29c9862190f766c7a34881867f9018d489fdd7
moulogun <moulogun@yandex.ru>
2024-07-08 22:03:12 +0300

    homework3.py

--- homework3.py
+++ homework3.py
@@ -5,5 +5,4 @@ print('Age:', age)
 age = (age + 1)
 print('New Age:', age)
 is_student = True
-print('is_student',type(is_student))
-print('Is Student:', is_student)
+print('Is Student:', is_student, type(is_student))
