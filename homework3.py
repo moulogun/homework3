02c4daa59b99df95d5c451f95cabe96ca882064f
moulogun <moulogun@yandex.ru>
2024-07-08 21:57:45 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = 'Антон'
+print('Name:', name)
+age = 24
+print('Age:', age)
+age = (age + 1)
+print('New Age:', age)
+is_student = True
+print('is_student',type(is_student))
+print('Is Student:', is_student)
